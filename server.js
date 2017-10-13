85a5fe1c1b8bd4c3d913a77a114b49574cd3aafd
James Tang <ulrichyu@outie.net>
2017-10-13 16:08:47 -0500

    fixed server error by adding function

--- server.js
+++ server.js
@@ -6,21 +6,15 @@ require('dotenv').config();
 // app setup
 const app = express();
 
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 
 // app routes
-// app.get('/', function() {
-//   res.send('THIS IS WORKING');
-// });
+app.get('/', function(req, res) {
+  res.send('THIS IS WORKING');
+});
 
 //start server
 app.listen(3000, function() {
   console.log('Server is listening on port 3000!')
 });
-=======
-
-app.set('view engine', 'ejs');
-app.use(bodyParser.json());
-
