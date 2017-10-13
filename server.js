ce1874e1a20608d5e4f49198754a869889ced9f9
James Tang <ulrichyu@outie.net>
2017-10-13 16:02:30 -0500

    added server listen to server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-
-
-
+// dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
+// app setup
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+
+// app routes
+// app.get('/', function() {
+//   res.send('THIS IS WORKING');
+// });
+
+//start server
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000!')
+});
