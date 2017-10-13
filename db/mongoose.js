22a006729cf78af05ce7556ac0d7c46505b786b9
Matt <mattray0295@gmail.com>
2017-10-13 15:58:45 -0500

    added mongoose connection

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,8 +3,12 @@ require('dotenv').config();
 
 // connect to Mongoose/Mongo DB
 mongoose.connection.openUri(process.env.MONGODB_URI ||process.env.DB_CONN, {}, (err, conn) => {
-    if (err) {  console.log('Error connecting to Mongo DB.', err); }
-    else     {  console.log('Mongoose successfully connected to Mongo DB.'); }
+    if (err) {
+        console.log('Error connecting to Mongo DB.', err);
+    }
+    else {
+        console.log('Mongoose successfully connected to Mongo DB.');
+    }
 });
 
 //add mongo DB error event
