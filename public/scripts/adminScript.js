7f47d2fbf06142f36f0b385660309bc7772857f8
Matt <mattray0295@gmail.com>
2017-10-16 14:23:46 -0500

    GET/POST work on admin page

--- public/scripts/adminScript.js
+++ public/scripts/adminScript.js
@@ -1,3 +1,84 @@
 
 
+$(function() {
+
+
+
+
+    function getItems() {
+        $.ajax({
+            method: 'GET',
+            url: 'http://localhost:3000/api/items',
+            dataType: 'json',
+            success: onGetItemSuccess,
+            // error: onError
+        });
+    }
+
+
+
+    function onGetItemSuccess(data) {
+
+        data.forEach((value) => {
+            $('.itemList').append("<p>Object ID: " + value._id + "</p>");
+            $('.itemList').append("<p>Name: " + value.name + "</p>");
+            $('.itemList').append("<p>Price: $" + value.price + "</p>");
+        });
+    }
+
+    $('#getFirstItem').on('click', (e) => {
+        getItems();
+    });
+
+
+
+
+
+    function postItems() {
+        $.ajax({
+            method: 'POST',
+            url: 'http://localhost:3000/api/items',
+            dataType: 'json',
+            data: $('#postData').serialize(),
+            success: onPostItemSuccess
+        });
+    }
+
+
+    function onPostItemSuccess(data) {
+        console.log(data);
+    }
+
+    $('#submitPost').on('click', (e) => {
+        e.preventDefault();
+        postItems();
+        $('.itemList p').empty();
+        getItems();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
 
