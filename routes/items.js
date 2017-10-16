925c0a290c44188599daf0c854a78aca6b202958
jaylara <jay.lara@outlook.com>
2017-10-16 12:39:33 -0500

    added storefront files

--- routes/items.js
+++ routes/items.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD:routes/item.js
-const DB = require('../models/index');
-
-//Selects one item by parameter id from specified DB.Item
-function selectItem(req, res) {
-	DB.Item.findOne({_id: req.params.id}, (err, fItem) => {
-		res.json(fItem);
-	});
-}//end of selectItem()
-
-//Selects all items from specified DB.Item
-function selectAllItems(req, res) {
-	DB.Item.find((err, items) => { // send all items as JSON response
-		if (err) { return console.log("index error: " + err); }
-		res.json(items);
-	});
-}//end of selectAllItems()
-
-//Creates a simple item. create item into the specified DB.Item
-function createItem(req, res) {
-    console.log(req.body);
-	(new DB.Item(req.body)).save((err, newItem) => {
-		res.json(newItem);
-	});
-}//end of createItem()
-
-//Updates one item by parameter id from specified DB.Item
-function updateItem(req, res) {
-	DB.Item.update({_id: req.params.id}, {$set: req.body}, {new:true}, (err, uItem) => {
-		if (err) { return console.log("index error: " + err); }
-		res.json(uItem);
-	});
-}//end of updateItem()
-
-//Deletes one item by parameter id from specified DB.Item
-function deleteItem(req, res) {
-	DB.Item.findOneAndRemove({ _id: req.params.id }).exec((err, dItem) => {
-		res.json(dItem);
-	});
-}//end of deleteItem()
-
-//exporting common, simple CRUD methods for use by other routes
-module.exports = {
-	selectAllItems : selectAllItems,
-	selectItem : selectItem,
-	createItem : createItem,
-	updateItem : updateItem,
-	deleteItem : deleteItem
-};
-=======
 const DB = require('../models');
 
 //Selects one item by parameter id from specified DB.Item
@@ -95,4 +45,3 @@ module.exports = {
 	updateItem : updateItem,
 	deleteItem : deleteItem
 };
->>>>>>> 6b0190a388824266521f072e7af8ab4a20baf6b5:routes/items.js
