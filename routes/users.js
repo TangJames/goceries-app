cfd378282c46e35ef32db6d846ab2005d377d3d7
James Tang <ulrichyu@outie.net>
2017-10-17 09:09:27 -0500

    changed users.js to match the project

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const DB = require('../models');
 
 //Selects one user by parameter id from specified DB.User
 function selectUser(req, res) {
-	DB.User.findOne({_id: req.params.id}, (err, fItem) => {
-		res.json(fItem);
+	DB.User.findOne({_id: req.params.id}, (err, fUser) => {
+		res.json(fUser);
 	});
 }//end of selectUser()
 
@@ -11,29 +11,29 @@ function selectUser(req, res) {
 function selectAllUsers(req, res) {
 	DB.User.find((err, users) => { // send all users as JSON response
 		if (err) { return console.log("index error: " + err); }
-		res.json(items);
+		res.json(users);
 	});
 }//end of selectAllUsers()
 
 //Creates a simple user. create user into the specified DB.User
 function createUser(req, res) {
-	(new DB.User(req.body)).save((err, newItem) => {
-		res.json(newItem);
+	(new DB.User(req.body)).save((err, newUser) => {
+		res.json(newUser);
 	});
 }//end of createUser()
 
 //Updates one user by parameter id from specified DB.User
 function updateUser(req, res) {
-	DB.User.update({_id: req.params.id}, {$set: req.body}, {new:true}, (err, uItem) => {
+	DB.User.update({_id: req.params.id}, {$set: req.body}, {new:true}, (err, uUser) => {
 		if (err) { return console.log("index error: " + err); }
-		res.json(uItem);
+		res.json(uUser);
 	});
 }//end of updateUser()
 
 //Deletes one user by parameter id from specified DB.User
 function deleteUser(req, res) {
-	DB.User.findOneAndRemove({ _id: req.params.id }).exec((err, dItem) => {
-		res.json(dItem);
+	DB.User.findOneAndRemove({ _id: req.params.id }).exec((err, dUser) => {
+		res.json(dUser);
 	});
 }//end of deleteUser()
 
